52113e8c7dd4ef1e9b6c4888817b8ac9f55a0335
IlikeBB <32098079+IlikeBB@users.noreply.github.com>
2021-12-28 11:56:28 +0800

    Add files via upload

--- core/classes.py
+++ core/classes.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-
-
+from core.firebase_config import *
 class Cog_Extension(commands.Cog):
         def __init__(self, bot):
             self.bot = bot
+            self.ref = ref
